333c3b506749e81856eb658690e6adae0a5d6258
TebbaaX <58398485+TebbaaX@users.noreply.github.com>
2023-03-05 14:12:39 +0100

    Save push, Evading error.

--- Prototypes/prototypebasic/app/src/main/java/com/example/prototype_basic/MainActivity.kt
+++ Prototypes/prototypebasic/app/src/main/java/com/example/prototype_basic/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // list -> input -> recycle view to display
+        val tasks = mutableListOf<String>("task 1", "task 2")
+        // to upgrade to API 33.
+
+
     }
 }
