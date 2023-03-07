606d73fe02fcccb50bb9110a7a3d5c6732c95bdb
TebbaaX <>
2023-03-08 00:29:25 +0100

    added Prototype basic v2.

---
+++ Prototypes/prototypebasic-v2/app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.recycleview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import android.widget.TextView
+import com.example.recycleview.adapter.ItemAdapter
+import com.example.recycleview.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+        val addAffirmationButton = findViewById<Button>(R.id.MainButton)
+
+        addAffirmationButton.setOnClickListener {
+            val affirmations = resources.getStringArray(R.array.affirmations)
+            val newAffirmation = "This is a new affirmation"
+            val newAffirmationsList = affirmations.toMutableList()
+            newAffirmationsList.add(newAffirmation)
+            val newAffirmationsArray = newAffirmationsList.toTypedArray()
+            resources.getStringArray(R.array.affirmations).copyInto(newAffirmationsArray)
+
+
+
+
+
+        }
+
+
+    }
+}
