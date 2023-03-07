06a9e54c64f57bed211e24941e29a677e66d642c
TebbaaX <>
2023-03-07 23:26:56 +0100

    Dropping Main Prototype basic.

--- Prototypes/prototypebasic/app/src/main/java/com/example/prototype_basic/MainActivity.kt
+++ Prototypes/prototypebasic/app/src/main/java/com/example/prototype_basic/MainActivity.kt
@@ -11,6 +11,5 @@ class MainActivity : AppCompatActivity() {
         val tasks = mutableListOf<String>("task 1", "task 2")
         // to upgrade to API 33.
 
-
     }
 }
---
+++ Prototypes/prototypebasic/app/src/main/java/com/example/prototype_basic/TodoData.kt
@@ -0,0 +1,5 @@
+package com.example.prototype_basic
+import java.util.*
+
+class TodoData (val name: String, val dueDate: Date, var isComplete: Boolean) {
+}
